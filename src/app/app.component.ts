becf1c4e773a855e578a6f3afd540e3ef57e8135
566850 <566850@PC361697.cts.com>
2018-05-11 15:59:33 +0530

    initial-commit: angular client - spring cloud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
+import {Observable} from 'rxjs/Observable'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  title = 'CTS Bank';
+  age:number;
+  found:boolean;
+  
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
+  
+  name:string ='';
+   constructor(private httpClient:HttpClient){}
+   onNamekeyup(event:any){
+	   console.log(event.target.value)
+	   this.name = event.target.value;
+	   this.found = false;
+   }
+   /*customer : string {userName: 'test', password: '455'};
+   
+   loginToApp(){
+	   console.log("call  from app component");
+	   this.httpClient.post('http://localhost:8080/customer/add',JSON.stringify(this.customer),{headers: this.headers})
+	   .subscribe(
+			  (data:any) =>{
+				  if(data){
+					  console.log(data);
+				  }
+			  }
+			   )
+   }*/
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { AddBeneficiaryComponent } from './add-beneficiary/add-beneficiary.component';
+import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
+import { HomeComponent } from './home/home.component';
+import { PaymentTransactionComponent } from './payment-transaction/payment-transaction.component';
+import {RouterModule,Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { ViewBenificiariesComponent } from './view-benificiaries/view-benificiaries.component';
 
+const appRoutes : Routes = [
+	{path:'home/:id', component:HomeComponent},
+	{path:'createAccount/:id', component:CreateAccountComponent},
+	{path:'addBene/:id', component:AddBeneficiaryComponent},
+	{path:'fundTransfer/:id', component:FundTransferComponent},
+	{path:'paymentTransaction/:id', component:PaymentTransactionComponent},
+	{path:'login', component:LoginComponent},
+	{path:'createCustomer', component:CreateCustomerComponent},
+	{path:'viewBene/:id', component:ViewBenificiariesComponent},
+	{path:'', redirectTo:'/login',pathMatch:'full'},
+	{path:'**', redirectTo:'/login',pathMatch:'full'}
+	
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    AddBeneficiaryComponent,
+    FundTransferComponent,
+    HomeComponent,
+    PaymentTransactionComponent,
+    CreateCustomerComponent,
+    ViewBenificiariesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+/*	$httpProvider.defaults.useXDomain = true;
+	$httpProvider.defaults.withCredentials = true; 
+*/}
